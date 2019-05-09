b8177d8c38a442aef6e0a7929e703361d80ea650
tinawong15 <tinaw425@gmail.com>
2019-05-09 18:07:17 -0400

    added register and login

--- app.py
+++ app.py
@@ -15,26 +15,72 @@ def index():
 
 @app.route("/register")
 def register():
-    return "hi"
+    return render_template('register.html')
+
+@app.route('/register_auth', methods = ['POST'])
+def register_auth():
+    '''This function checks the form on the signup page and calls register() to register the user into the database.'''
+    username = request.form['username']
+    password = request.form['password']
+    name = request.form['name']
+    confirmed_pass = request.form['confirmedPassword']
+    if username == "":
+        flash("Please make sure to enter a username!")
+        return redirect(url_for('register'))
+    elif password == "":
+        flash("Please make sure to enter a password!")
+        return redirect(url_for('register'))
+    elif password != confirmed_pass: # checks to make sure two passwords entered are the same
+        flash("Please make sure the passwords you enter are the same.")
+        return redirect(url_for('register'))
+    else:
+        if create_db.register(username, password, name):
+            flash("You have successfully registered.")
+        else:
+            flash("Please enter another username. The one you entered is already in the database.")
+            return redirect(url_for('register'))
+    return redirect('/login')
 
 @app.route("/login")
-def register():
-    return "hi"
+def login():
+    '''This function renders the HTML template for the login page.'''
+    return render_template('login.html')
+
+@app.route('/login_auth', methods = ['POST'])
+def login_auth():
+    '''This function calls authenticate() to check if the form's username and password match the database. If successful, this function redirects the user to the home page.'''
+    username = request.form['username']
+    password = request.form['password']
+
+    if create_db.authenticate(username, password):
+        session['username'] = username
+        flash("You have successfully logged in.")
+        return redirect('/')
+    else:
+        flash("Invalid username and password combination")
+        return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    '''This function removes the username from the session, logging the user out. Redirects user to home page.'''
+    session.pop('username') # ends session
+    flash("You have successfully logged out.")
+    return redirect('/')
 
 @app.route("/search")
-def register():
+def search():
     return "hi"
 
 @app.route("/user")
-def register():
+def user():
     return "hi"
 
-@app.route("/financial")
-def register():
+@app.route("/fin_aid")
+def fin_aid():
     return "hi"
 
 @app.route("/college")
-def register():
+def college():
     return "hi"
 
 if __name__ == "__main__":
--- util/create_db.py
+++ util/create_db.py
@@ -1,5 +1,6 @@
 import sqlite3
 import os
+from passlib.hash import sha256_crypt
 
 DIR = os.path.dirname(__file__) or '.'
 DIR += '/../' # points to util, ../ to go back to Flask root
@@ -37,7 +38,7 @@ def setup():
 
     db = sqlite3.connect(DATABASE_LINK)
     c = db.cursor()
-    command = "CREATE TABLE IF NOT EXISTS student (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, interests TEXT, username TEXT NOT NULL UNIQUE, password TEXT NOT NULL)"
+    command = "CREATE TABLE IF NOT EXISTS student (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, interests TEXT, username TEXT NOT NULL, password TEXT NOT NULL)"
     c.execute(command)
     command = "CREATE TABLE IF NOT EXISTS college (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, deadline TEXT NOT NULL, financial_aid STRING NOT NULL, submitted BOOLEAN NOT NULL, additional_info STRING, student_id INTEGER NOT NULL)"
     c.execute(command)
@@ -47,3 +48,31 @@ def setup():
     c.execute(command)
     db.commit()
     db.close()
+
+def register(username, password, name):
+    '''This function adds the user to the database.
+    If username already exists, returns false. Otherwise, the function inserts a row in users and returns true.'''
+    db = sqlite3.connect(DATABASE_LINK)
+    c = db.cursor()
+    data = c.execute("SELECT * FROM student;")
+    for row in data:
+        if username == row[2]:
+            return False
+    params = (username, sha256_crypt.hash(password), name)
+    c.execute("INSERT INTO student (username, password, name) VALUES (?, ?, ?)", params)
+
+    db.commit()
+    db.close()
+    return True
+
+def authenticate(username, password):
+    '''This function checks user login. If username and encrypted password match, the function returns true. The function returns false otherwise.'''
+    db = sqlite3.connect(DATABASE_LINK)
+    c = db.cursor()
+    data = c.execute("SELECT * FROM student")
+    for row in data:
+        if row[3] == username and sha256_crypt.verify(password, row[4]):
+            db.close()
+            return True
+    db.close()
+    return False
